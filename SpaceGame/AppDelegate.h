044ce83fb6a7cc59d525ce01b388444d9b32c943
Ray Wenderlich <ray.wenderlich@gmail.com>
2012-11-22 16:36:01 -0500

    Emptied template and converted to ARC.

--- SpaceGame/AppDelegate.h
+++ SpaceGame/AppDelegate.h
@@ -15,14 +15,14 @@
 
 @interface AppController : NSObject <UIApplicationDelegate>
 {
-	UIWindow *window_;
-	MyNavigationController *navController_;
-	
-	CCDirectorIOS	*director_;							// weak ref
+    UIWindow *window_;
+    MyNavigationController *navController_;
+    
+    CCDirectorIOS	*__unsafe_unretained director_;							// weak ref
 }
 
-@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, strong) UIWindow *window;
 @property (readonly) MyNavigationController *navController;
-@property (readonly) CCDirectorIOS *director;
+@property (unsafe_unretained, readonly) CCDirectorIOS *director;
 
 @end
