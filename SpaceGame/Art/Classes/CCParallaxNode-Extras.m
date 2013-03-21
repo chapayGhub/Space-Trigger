5af4b6abe628d3bb231a74dc345aecc21d0421c4
Jonny Ramos <jramos@uchicago.edu>
2013-03-21 17:35:22 -0500

    SpaceTrigger Final

--- SpaceGame/Art/Classes/CCParallaxNode-Extras.m
+++ SpaceGame/Art/Classes/CCParallaxNode-Extras.m
@@ -13,13 +13,8 @@
 
 -(void) incrementOffset:(CGPoint)offset forChild:(CCNode*)node
 {
-    for( unsigned int i=0;i < parallaxArray_->num;i++) {
-        CGPointObject *point = parallaxArray_->arr[i];
-        if( [[point child] isEqual:node] ) {
-            [point setOffset:ccpAdd([point offset], offset)];
-            break;
-        }
-    }
+
+    
 }
 
 @end
